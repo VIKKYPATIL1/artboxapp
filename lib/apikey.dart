b525076516cc9fc0f6794d06e13aa4192f604375
Chetan Pravin Kachave <91623886+VIKKYPATIL1@users.noreply.github.com>
2024-06-04 19:16:13 +0530

    Update apikey.dart

--- lib/apikey.dart
+++ lib/apikey.dart
@@ -1,4 +1,2 @@
-const String aIKey =
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IjY3NTM0NzJmMTE2ZDMwMTg0ZjFiNDg3NjgzNDg5MzM0IiwiY3JlYXRlZF9hdCI6IjIwMjQtMDYtMDNUMTM6NDc6NDAuODE5NzM2In0.UCLaPiEo7ol1mkoZ-Z-rPOI0w9CtssfhtmuEHCzJyak";
-
+const String aIKey ="Enter you api key";
 const String endpoint = 'https://api.monsterapi.ai/v1/generate/txt2img';
